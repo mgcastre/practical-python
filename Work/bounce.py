a981ff6c4dadc969ae2507d93333d271c36329c0
mgcastre <mariag.castrellon@gmail.com>
2021-10-04 15:41:02 -0500

    solved exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
-#
-# Exercise 1.5
+# M. G. Castrellon
+# 10/04/2021
+
+# Exercise 1.5: The Bouncing Ball
+
+height = 100
+
+for i in range(10):
+	height = (3/5)*height
+	# print("Height after %1.0f bounce(s): %0.1f" % (i+1, height))
+	print("Height after %1.0f bounce(s):" % (i+1))
+	print(round(height, 4))
+	print("\n")
