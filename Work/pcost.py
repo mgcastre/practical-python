7406a6883ea8ff43b3919ce2561ff6b3ae0649f2
M. G. Castrellón <mariag.castrellon@gmail.com>
2022-11-16 18:08:13 +0100

    Completed Ex. 1.30 - turning script into function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,20 +1,23 @@
 # pcost.py
-# Exercise 1.27
-# Reading a data file
+# Exercise 1.30
+# Turning script into a function
 # M. G. Castrellon
 # 16 Nov 2022
 
 import numpy as np
 
-file = open('Data/portfolio.csv', 'rt')
-headers = next(file).split(',')
-share_cost = []
-for line in file:
-	row = line.split(',')
-	number_of_shares = float(row[1])
-	single_share_cost = float(row[2])
-	p = number_of_shares*single_share_cost
-	share_cost.append(p)
-file.close()
-total_cost = np.sum(share_cost)
+def portfolio_cost(filename):
+	file = open(filename, 'rt')
+	headers = next(file).split(',')
+	share_cost = []
+	for line in file:
+		row = line.split(',')
+		number_of_shares = float(row[1])
+		single_share_cost = float(row[2])
+		p = number_of_shares*single_share_cost
+		share_cost.append(p)
+	file.close()
+	return np.sum(share_cost)
+
+total_cost = portfolio_cost('Data/portfolio.csv')
 print('Total cost: ', total_cost)
