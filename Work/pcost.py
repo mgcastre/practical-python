9970e816cc87eec1aa8e1346e7d2e5d54545ca01
M. G. Castrellón <mariag.castrellon@gmail.com>
2022-11-21 17:09:50 +0100

    I'm stuck in Ex. 1.31

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,6 +1,6 @@
 # pcost.py
-# Exercise 1.30
-# Turning script into a function
+# Exercise 1.31
+# Error handling
 # M. G. Castrellon
 # 16 Nov 2022
 
@@ -9,15 +9,27 @@ import numpy as np
 def portfolio_cost(filename):
 	file = open(filename, 'rt')
 	headers = next(file).split(',')
-	share_cost = []
+	list_share_cost = []
 	for line in file:
 		row = line.split(',')
-		number_of_shares = float(row[1])
-		single_share_cost = float(row[2])
-		p = number_of_shares*single_share_cost
-		share_cost.append(p)
+		try:
+			number_of_shares = int(row[1])
+			single_share_cost = float(row[2])
+			p = number_of_shares*single_share_cost
+		except ValueError:
+			print("Failed to parse")
+			# pass
+			# raise RuntimeError("Failed to parse")
+		# single_share_cost = float(row[2])
+		# p = number_of_shares*single_share_cost
+		list_share_cost.append(p)
 	file.close()
-	return np.sum(share_cost)
+	cost = np.sum(list_share_cost)
+	return cost, list_share_cost
 
-total_cost = portfolio_cost('Data/portfolio.csv')
-print('Total cost: ', total_cost)
+print()
+total_cost, table = portfolio_cost('Data/missing.csv')
+print("Total price per share:")
+for i in range(len(table)):
+	print(table[i])
+print('\nTotal cost: ', total_cost)
