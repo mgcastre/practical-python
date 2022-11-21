54470286657a0954a817fe5879aadc2b58658182
M. G. Castrellón <mariag.castrellon@gmail.com>
2022-11-21 17:55:42 +0100

    Solved Ex 1.31 (Section 1.7)

--- Work/pcost.py
+++ Work/pcost.py
@@ -15,21 +15,25 @@ def portfolio_cost(filename):
 		try:
 			number_of_shares = int(row[1])
 			single_share_cost = float(row[2])
-			p = number_of_shares*single_share_cost
 		except ValueError:
-			print("Failed to parse")
-			# pass
-			# raise RuntimeError("Failed to parse")
-		# single_share_cost = float(row[2])
-		# p = number_of_shares*single_share_cost
-		list_share_cost.append(p)
+			print("Failed to parse, row skipped.")
+		else:
+			p = number_of_shares*single_share_cost
+			list_share_cost.append(p)
 	file.close()
 	cost = np.sum(list_share_cost)
 	return cost, list_share_cost
 
-print()
-total_cost, table = portfolio_cost('Data/missing.csv')
-print("Total price per share:")
+doc_path = 'Data/missing.csv'
+total_cost, table = portfolio_cost(doc_path)
+print('\nTotal cost: ', total_cost)
+
+print("\nTotal price per share:")
 for i in range(len(table)):
 	print(table[i])
-print('\nTotal cost: ', total_cost)
+
+print('\nOriginal Data')
+with open(doc_path, 'rt') as f:
+	data = f.read()
+	print(data)
+
